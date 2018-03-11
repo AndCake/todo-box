9d5e65c4373c6fbd73063bc66e8b8552b42af3ac
Robert Kunze <r.kunze@mobizcorp.de>
2018-03-11 14:54:58 +0100

    added basic service-worker support

--- pages/todo-app.js
+++ pages/todo-app.js
@@ -68,6 +68,12 @@ export default class TodoApp {
 				this.getHost().setProps('credentials', data);
 			}
 		});
+
+		if (typeof navigator !== 'undefined' && 'serviceWorker' in navigator) {
+			window.addEventListener('load', () => {
+				navigator.serviceWorker.register('/sw.js');
+			});
+		}
 	}
 
 	onunmount() {
